4d109ab98e1579f05b31271fee2225377c30ac95
akhiljerald <xakhilytarunz@gmail.com>
2023-09-24 13:52:15 +0530

    incremental update

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
-    return "<p>Login</p>"
-    #return render_template('login.html')
+    return render_template('login.html')
 
-@auth.route('/logout')
+@auth.route('/logout', methods=['GET','POST'])
 def logout():
-    return "<p>Logout</p>"
-    #return '<p>Logout</p>'
+    return render_template('logout.html')
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET','POST'])
 def sign_up():
-    return "<p>Sign Up</p>"
-    #return render_template('sign_up.html')
+    
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            flash('Adding User to Database.', category='success')
+            
+    return render_template('signup.html')

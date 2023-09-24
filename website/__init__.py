c4ccf7b248c769a45c01b96d0964ae5c24d0364f
akhiljerald <xakhilytarunz@gmail.com>
2023-09-24 16:11:23 +0530

    complete working commit

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager #LoginManager is a class that will handle all of the sessions for us   
+
 db = SQLAlchemy()
 DB_NAME = 'database.db'
 
@@ -11,20 +13,30 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
     
-    
+       
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+   
     from .models import User, Note
     
     create_database(app)
+    
+    login_manager = LoginManager()  
+    login_manager.login_view = 'auth.login' # redirect to login page if user is not logged in
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader # decorator
+    def load_user(id):
+        return User.query.get(int(id)) # get user id from database
 
     return app
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+      with app.app_context():        
+          db.create_all()
+          print('Created Database!')
